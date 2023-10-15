28b942d55a54aaa270b934dd3c6acf7f23e084e5
Nigel Abrera <abrera.nigel0026@gmail.com>
2023-10-14 19:44:45 -0500

    Updating React Lesson

--- React_Info_Site/index.js
+++ React_Info_Site/index.js
@@ -4,20 +4,20 @@
  * @date 10/14/2023
  */
 
+/*
+Generating an h1 tag, first react code.
+ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById("root"))
 
-// Generating an h1 tag, first react code.
-// ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById("root"))
+Practice #1, NOTE: it overwrites the first one.
+ReactDOM.render(<p>This is my first React Line</p>, document.getElementById("root"))
 
-// Practice #1, NOTE: it overwrites the first one.
-// ReactDOM.render(<p>This is my first React Line</p>, document.getElementById("root"))
 
-/* 
 Practice # 2
 Try to write that 1-liner of React code again! This time, see if you can figure out how to
 render an <ul> with 2+ <li>s inside
-*/
 
-/*
+
+
 ReactDOM.render(
     <ul>
         <li>Test 1</li>
@@ -25,10 +25,9 @@ ReactDOM.render(
         <li>Test 3</li>
     </ul>, document.getElementById("root")
 )
-*/
 
-// Vids answer
-/*
+Vid's answer
+
 ReactDOM.render(
     <ul>
         <li>Thing 1</li>
@@ -36,19 +35,18 @@ ReactDOM.render(
     </ul>,
     document.getElementById("root")
 )
-*/
 
-// Rendering some content after a tag  
-/*
+Rendering some content after a tag  
+
 function MainContent() {
     return (
         <h1>I'm learning React</h1>
     )
 }
-*/
 
-// Calling the function like a tag
-/*
+
+Calling the function like a tag
+
 ReactDOM.render(
     <div>
         <NavBar />
@@ -56,9 +54,9 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 )
-*/
 
-/*
+
+
 Challenge - recreate the above line of code in vanilla JS
 by creating and appending an h1 to our div#root (without using innerHTML).
 
@@ -66,31 +64,65 @@ by creating and appending an h1 to our div#root (without using innerHTML).
 - Give it some textContent
 - Give it a class name of "header"
 - append it as a child of the div#root
-*/
 
-// Note: it doesn't work when you have the type="text/babel" in script tag
-/*
+
+Note: it doesn't work when you have the type="text/babel" in script tag
+
 let h1 = document.createElement("h1");
 h1.textContent = "This is vanilla JS";
 h1.classList.add("header");
 let root = document.getElementById("root");
 root.appendChild(h1);
-*/
 
-/*
+
+
 Vid's answer
 
 const h1 = document.createElement("h1");
 h1.textContent = "This is an imperative way to program";
 h1.className = "header";
 document.getElementById("root").appendChild(h1);
-*/
 
-/* 
 We use react to be declarative since vanilla JS is an imperative way to code
 
 ReactDOM.render(<h1 className="header">Hello, React!</h1>, doocument.getElementById("root"))
+
+JSX
+Sample syntaxes are, instead of using class we use className
+
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").appendChild(h1);
+console.log(h1); The output would be <h1 class="header">
+
+const element = <h1 className="header">This is JSX</h1>;
+ReactDOM.render(element, document.getElementById("root"));
+
+console.log(element); The output would be below
+{
+    type: "h1",
+    key: null,
+    ref: null,
+    props: {
+        className: "header", 
+        children: "This is JSX"
+    },
+    _owner: null,
+    _store: {}
+}
+
+Dev Notes: it could only render 1 parent element.
+
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div
+)
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
 */
-
-
-
